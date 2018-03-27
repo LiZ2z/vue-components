3df4499e24bb18fc264a1a0b2055ced30e5bf195
LiZ2z <30578440+LiZ2z@users.noreply.github.com>
2018-03-27 17:50:57 +0800

    2018-3-27

--- img-upload/img-upload.vue
+++ img-upload/img-upload.vue
@@ -8,48 +8,138 @@
       <div class="grid">
         <input name="fff" type="file" accept="image/*"
                id="update-input"
-               @change="updateFile($event)"
-               :disabled="this.files.length >=9"
+               @change="uploadImg($event)"
+               :disabled="this.imgs.length >=9"
                multiple>
         <label class="update-label" for="update-input">+</label>
       </div>
-      <div class="grid" v-for="(img, i) in files">
+
+      <div class="grid" v-for="(img, i) in imgs" :key='i' v-if="img">
         <div class="preview-img-wrap" @click="remove(i)">
-          <img class="preview-img" :src="handlerImg(img)" :alt="img.name">
+          <img class="preview-img" :src="img.src" :alt="img.alt">
           <span class="remove-btn">+</span>
         </div>
       </div>
+
     </div>
+    <canvas ref="canvas" width="750" class="compress-canvas"></canvas>
+
   </div>
 </template>
 
 <script>
+  import EXIF from './exif'
   export default {
     name: 'file-update',
     data () {
       return {
-        files: [],
+        imgs: [],
+        previews: []
       }
     },
+    mounted() {
+      window.URL = window.URL || window.webkitURL
+      let canvas = this.$refs.canvas
+      this.canvas = canvas
+      this.ctx = canvas.getContext('2d')
+      EXIF.call(this)
+    },
     methods: {
-      updateFile (e) {
-        let arr = this.files
-        arr.push(...e.target.files)
-        this.files = arr.slice(0,9)
+      uploadImg (e) {
+        const len = this.imgs.length
+        let newImgs = [...e.target.files]
+
+        newImgs = newImgs.slice(0, 9 - len)
+
+        this.handlerAdd(newImgs)
+        // this.handlerPreview(newImgs)
       },
-      handlerImg(v) {
-        let URL = window.URL || window.webkitURL;
-        return URL.createObjectURL(v)
+      handlerAdd(imgs) {
+        let imgFile = null, blobSrc = ''
+        const oldLen = this.imgs.length
+        const len = imgs.length
+        let arr = new Array(len)
+
+        this.imgs = this.imgs.concat(arr)
+
+        for(let i = 0; i < len; i++) {
+          imgFile = imgs[i]
+          blobSrc = window.URL.createObjectURL(imgFile)
+
+          // 获取图片拍摄时的旋转角度
+          this.EXIF.getData(imgFile, (function(vm){
+            return function () {
+              // 压缩图片
+              vm.compress(blobSrc, vm.EXIF.getTag (this, 'Orientation'), oldLen + i)
+            }
+          })(this))
+
+        } // END for
       },
+      compress(src, ori, i) {
+        let img = new Image()
+        img.src = src
+        img.onload = (function (vm, ori, i) {
+          return function () {
+            this.onload = null
+            window.URL.revokeObjectURL(this.src)
+
+            const w = 750,
+              h = (this.height/this.width) * w,
+              canvas = vm.canvas,
+              ctx = vm.ctx
+
+            canvas.width = w
+            canvas.height = h
+
+            let deg = 0,
+              drawH = h,
+              drawW = w
+
+            switch (ori) {
+
+              case 3:
+                deg=Math.PI
+                drawW = -w
+                drawH = -h
+                break;
+              case 6:
+                canvas.width = h
+                canvas.height = w
+                drawH = -h
+                deg = Math.PI/2
+                break;
+              case 8:
+                canvas.width = h
+                canvas.height = w
+                deg = Math.PI*3 / 2
+                drawW = -w
+                break;
+              default:
+                break;
+            }
+
+            ctx.rotate(deg)
+            ctx.drawImage(this,0,0,drawW, drawH)
+
+            vm.imgs.splice(i, 1, {
+              src: canvas.toDataURL('image/jpeg')
+            })
+
+          }
+        })(this, ori, i)
+
+      }, // END compress  method
       remove(i) {
-        this.files.splice(i,1)
+        this.imgs.splice(i, 1)
+        this.previews.splice(i, 1)
       },
       submit(e) {
         /*this.$axios({
           methods: 'post',
           url: '/img',
           data: {
-            fff: this.files[0]
+            fff: this.imgs[0]
           },
           headers: {'enctype': 'multipart/form-data'}
         })
@@ -58,23 +148,24 @@
         })*/
         let xhr = new XMLHttpRequest()
         let formData = new FormData();
-        let arr = this.files
+        let arr = this.imgs
         for(let i = 0,l = arr.length; i < l; i ++) {
-          formData.append('fff', arr[0]);
-
+          formData.append('fff', arr[i]);
+          console.log(arr[i])
         }
-        console.log(formData)
         // xhr.onload = uploadSuccess;
         // xhr.upload.onprogress = setProgress;
         xhr.open('post', '/img', true);
         xhr.send(formData);
-
       }
     }
+
   }
 </script>
 
 <style >
+  .compress-canvas{display:none;}
+
   .clearfix:after{
     display: block;
     content: '\0020';
@@ -107,6 +198,7 @@
     line-height:58px;
   }
   #update-input{display: none;}
+
   #update-input:disabled+.update-label{
     border: 1px dashed #aaa;
     color:#aaa;
